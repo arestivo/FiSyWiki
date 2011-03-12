1a3310b73505e65c22bc0d5e09b663892c6d191b
André Restivo <arestivo@arestivo-laptop.(none)>
2011-03-12 19:49:50 +0000

    Automatically redirect to help page if index page does not exist.

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
   include_once('lib/setup.php');
 
   $path = Page::getPath();
+
+  if ($path == 'index' && !file_exists('pages/index') && file_exists('pages/help/index')) {
+    header("Location: ?p=help");
+    die;
+  }
+
   $page = Page::create($path);
   $page->render();
 ?>
