d54ec6bffc59c192daa5464d16273e6897bd3e9d
André Restivo <andre.restivo@gmail.com>
2011-03-19 12:44:01 +0000

    Changed latex folder to tmp/latex. Added warning when unable to create or use folder. Automatic folder creation.

--- widgets/latex.php
+++ widgets/latex.php
@@ -7,23 +7,29 @@
       if ($value == null) return;
 
       $hash = md5($value);
+
+      if (!file_exists('tmp/latex/')) 
+        if (!@mkdir('tmp/latex/', 0755, true)) 
+          return 'Latex widget needs folder /tmp/latex with writing permissions<br />';
       
-      if (!file_exists('tmp/'.$hash.'.png')) {
+      if (!file_exists('tmp/latex/'.$hash.'.png')) {
       
 	      $latex = '\documentclass[fleqn]{article} \usepackage{amssymb,amsmath,bm,color} \usepackage[latin1]{inputenc} \begin{document} \thispagestyle{empty} \mathindent0cm \parindent0cm \begin{displaymath}'.$value.'\end{displaymath}\end{document}';
 
-	      $file = 'tmp/'.$hash.'.';
+	      $file = 'tmp/latex/'.$hash.'.';
 
-	      $fh = fopen($file.'latex', 'w');
+	      $fh = @fopen($file.'latex', 'w');
+	      if (!$fh) return 'Latex widget needs folder /tmp/latex with writing permissions<br />';
+	      
 	      fwrite($fh, $latex);
 	      fclose($fh);
 	      
-	      exec('cd tmp; latex '.$hash.'.latex');
-	      exec('cd tmp; dvipng q -T tight -D 130 -o '.$hash.'.png '.$hash.'.dvi');
-	      exec('cd tmp; rm *.latex; rm *.dvi; rm *.aux; rm *.log');
+	      exec('cd tmp/latex; latex '.$hash.'.latex');
+	      exec('cd tmp/latex; dvipng q -T tight -D 130 -o '.$hash.'.png '.$hash.'.dvi');
+	      exec('cd tmp/latex; rm *.latex; rm *.dvi; rm *.aux; rm *.log');
       }
             
-      return '<img class="latex" src="tmp/'.$hash.'.png" />';
+      return '<img class="latex" src="tmp/latex/'.$hash.'.png" />';
     }
   }
 ?>
